5b630893152b591351159b16ef77102c4b096d40
Jared <7060603@hotmail.ca>
2019-03-05 00:54:35 -0500

    Added `!Template`
    Replaces `!showhyphen` and `!gameonly`.
    The bot will still use those settings if they are in the database, but you cannot set them anymore. Instead, template will replace the old settings in the database.

--- channelchanger.js
+++ channelchanger.js
@@ -2,12 +2,12 @@ const Discord = require('discord.js');const client = new Discord.Client();
 const jsonfile = require('jsonfile');
 client.on('ready', () => {
 	console.log('Ready!');
-	client.user.setPresence({ game: { name: "!help", type: 0 } });
+	client.user.setPresence({ game: { name: "!help - Changing Channels", type: 0 } });
 	//list connected guilds and their ID's
 	for(var i=0;i<client.guilds.array().length;i++){
 		console.log(client.guilds.array()[i].name+" id: "+client.guilds.array()[i].id)
-    }
-    setTimeout(scanAll,4000)
+	}
+	setTimeout(scanAll,4000)
 });
 const tokens = require('./tokens.js');
 client.login(tokens.bot_token);
@@ -22,8 +22,8 @@ var keys=Object.keys(channels) //list of channel id's
 */
 function autosave() {
 	if(!changes){
-        	setTimeout(save,30000);
-        	changes=true
+		setTimeout(save,30000);
+		changes=true
 	}
 }
 /*
@@ -79,6 +79,10 @@ function scanAll(){
 // channelId: the id of the voice channel in question.
 function scanOne(channelId){
 	var channel=client.channels.get(channelId);
+	var channelArray=[]
+	if(channels[channelId]){
+		channelArray=channels[channelId]
+	}
 	if(channel){
 		if(channel.manageable){ //if the bot has permission to change the name
 			if(channel.members.firstKey()){ // if anyone is in the channel
@@ -90,22 +94,20 @@ function scanOne(channelId){
 						}
 					}
 				}
-				var newTitle=majority(games,channel.members.array().length,channels[channelId][1] || 0.5)
+				var newTitle=majority(games,channel.members.array().length,channelArray[1] || 0.5)
 				if(!noFlyList.includes(newTitle)){
-					if(channels[channelId][2]===1){
+					if(channelArray[2]===1){  //template or 
 						channel.setName(newTitle)
-					}else{
-						if(channels[channelId][3]===1){
-							channel.setName(channels[channelId][0]+" "+newTitle)
-						}else{
-							channel.setName(channels[channelId][0]+" - "+newTitle)
-						}
+					}else if(channelArray[3]===1){
+						channel.setName(channelArray[0]+" "+newTitle)
+					}else if(channelArray[2]){
+						channel.setName(channelArray[2].replace(/X/,channelArray[0]).replace(/Y/,newTitle))
 					}
 				}else{
-					channel.setName(channels[channelId][0])
+					channel.setName(channelArray[0])
 				}
 			}else{
-				channel.setName(channels[channelId][0])
+				channel.setName(channelArray[0])
 			}
 		}
 	}else{
@@ -131,22 +133,88 @@ client.on('voiceStateUpdate', (oldMember,newMember) => {
 })
 
 client.on('message', message =>{
-	var messageL=message.content.toLowerCase()
-	if (message.guild){
-		if (messageL==="!addvc"){
-			if(message.guild.me.hasPermission("MANAGE_CHANNELS")){
-				if(message.member.hasPermission("MANAGE_CHANNELS")){
-					if (message.member.voiceChannel){
-						try{
-							if (!channels[message.member.voiceChannel.id]){
-								channels[message.member.voiceChannel.id]=[message.member.voiceChannel.name];
-								autosave()
-								message.reply("Successfully added `"+message.member.voiceChannel.name+"` to my list")
-							}else{
-								message.reply("`"+channels[message.member.voiceChannel.id][0]+"` is already on my list.")
+	if(message.guild){
+		if(message.content[0]==="!"){  //peak efficiency
+			var messageL=message.content.toLowerCase()
+			if (messageL==="!addvc"){
+				if(message.guild.me.hasPermission("MANAGE_CHANNELS")){
+					if(message.member.hasPermission("MANAGE_CHANNELS")){
+						if (message.member.voiceChannel){
+							try{
+								if (!channels[message.member.voiceChannel.id]){
+									channels[message.member.voiceChannel.id]=[message.member.voiceChannel.name];
+									autosave()
+									message.reply("Successfully added `"+message.member.voiceChannel.name+"` to my list")
+								}else{
+									message.reply("`"+channels[message.member.voiceChannel.id][0]+"` is already on my list.")
+								}
+							}catch(err){
+								console.log(err)
 							}
-						}catch(err){
-							console.log(err)
+						}else{
+							message.reply("You must be in a voice channel to use this command.")
+						}
+					}else{
+						message.reply("You need `manage_channels` permission to do this.")
+					}
+				}else{
+					message.reply("I need `manage_channels` permission to do this.")
+				}
+			}
+			else if(messageL==="!removevc"){
+				if(message.guild.me.hasPermission("MANAGE_CHANNELS")){
+					if(message.member.hasPermission("MANAGE_CHANNELS")){
+						if (message.member.voiceChannel){
+							try{
+								if (channels[message.member.voiceChannel.id]){
+									message.member.voiceChannel.setName(channels[message.member.voiceChannelID][0])
+									delete channels[message.member.voiceChannelID];
+									autosave()
+									message.reply("Successfully removed `"+message.member.voiceChannel.name+"` from my list.")
+								}else{
+									message.reply("`"+message.member.voiceChannel.name+"` was not on my list.")
+								}
+							}catch(err){
+								console.log(err)
+							}
+						}else{
+							message.reply("You must be in a voice channel to use this command!")
+						}
+					}else{
+						message.reply("You need `manage_channels` permission to do this.")
+					}
+				}else{
+					message.reply("I need `manage_channels` permission to do this.")
+				}
+			}
+			else if(messageL==="!gameonly"){
+				message.reply("`!gameonly` has been replaced with `!template`. Check `!help` for details.")
+			}else if(messageL==="!template"){
+				if(message.member.voiceChannel){
+					if(channels[message.member.voiceChannelID]){
+						message.reply("Template for `"+channels[message.member.voiceChannelID][0]+"` is `"+channels[message.member.voiceChannelID][2]+"`")
+					}
+				}
+			}
+			else if(messageL.indexOf("!template ")===0){
+				if(message.member.hasPermission("MANAGE_CHANNELS")){
+					if(message.member.voiceChannel){
+						if(channels[message.member.voiceChannelID]){
+							var newTemplate=message.content.substr(10).trim()
+							if(newTemplate.length<100){
+								if(newTemplate.includes("Y")){
+									channels[message.member.voiceChannelID][2]=newTemplate
+									delete channels[message.member.voiceChannelID][3] //Get rid of archaic stuff. Not needed if using fresh database.
+									message.reply("The template for `"+channels[message.member.voiceChannelID][0]+"` is now "+newTemplate)
+									autosave()
+								}else{
+									message.reply("The template must include `Y`.")
+								}
+							}else{
+								message.reply("The template must be less than 100 characters long.")
+							}
+						}else{
+							message.reply("Please run `!addvc` first.")
 						}
 					}else{
 						message.reply("You must be in a voice channel to use this command.")
@@ -154,25 +222,31 @@ client.on('message', message =>{
 				}else{
 					message.reply("You need `manage_channels` permission to do this.")
 				}
-			}else{
-				message.reply("I need `manage_channels` permission to do this.")
 			}
-		}
-		else if (messageL==="!removevc"){
-			if(message.guild.me.hasPermission("MANAGE_CHANNELS")){
+			else if(messageL==="!showhyphen"){
+				message.reply("`!showhyphen` has been replaced with `!template`. Check `!help` for details.")
+			}
+			else if(messageL==="!majority"){
+				message.reply("Missing value. Example: `!majority 50`")
+			}
+			else if(messageL.indexOf("!majority ")===0){
 				if(message.member.hasPermission("MANAGE_CHANNELS")){
-					if (message.member.voiceChannel){
-						try{
-							if (channels[message.member.voiceChannel.id]){
-								message.member.voiceChannel.setName(channels[message.member.voiceChannel.id][0])
-								delete channels[message.member.voiceChannel.id];
-								autosave()
-								message.reply("Successfully removed `"+message.member.voiceChannel.name+"` from my list.")
+					if(message.member.voiceChannel){
+						if(channels[message.member.voiceChannelID]){
+							var majority=parseInt(messageL.substr(10))
+							if(majority){
+								if(majority>0 && majority <101){
+									channels[message.member.voiceChannelID][1]=majority/100
+									message.reply("Set majority for channel `"+channels[message.member.voiceChannelID][0]+"` to "+majority)
+									autosave()
+								}else{
+									message.reply("Invalid input. Number must be between 1 and 100.")
+								}
 							}else{
-								message.reply("`"+message.member.voiceChannel.name+"` was not on my list.")
+								message.reply("Invalid input.")
 							}
-						}catch(err){
-							console.log(err)
+						}else{
+							message.reply("Please run `!addvc` first.")
 						}
 					}else{
 						message.reply("You must be in a voice channel to use this command!")
@@ -180,85 +254,10 @@ client.on('message', message =>{
 				}else{
 					message.reply("You need `manage_channels` permission to do this.")
 				}
-			}else{
-				message.reply("I need `manage_channels` permission to do this.")
 			}
-		}
-		else if (messageL==="!gameonly"){
-			if(message.member.hasPermission("MANAGE_CHANNELS")){
-				if(message.member.voiceChannel){
-					if(channels[message.member.voiceChannelID]){
-						if(channels[message.member.voiceChannelID][2]){
-							delete channels[message.member.voiceChannelID][2]
-							message.reply("Game only is now off for `"+channels[message.member.voiceChannelID][0]+"`.")
-						}else{
-							channels[message.member.voiceChannelID][2]=1
-							message.reply("Only the game will be displayed for `"+channels[message.member.voiceChannelID][0]+"`. ")
-						}
-						autosave()
-					}else{
-						message.reply("Please run `!addvc` first.")
-					}
-				}else{
-					message.reply("You must be in a voice channel to use this command.")
-				}
-			}else{
-				message.reply("You need `manage_channels` permission to do this.")
+			else if (messageL==="!help"){
+				message.channel.send("__Channel Changer Help__\n*The purpose of Channel Changer is to add what game you're playing to your connected voice channel's name.*\n**!addvc**: Adds your voice channel to be renamed.\n**!removevc**: Removes your voice channel from the list.\n**!majority**: Sets what percentage of people have to be playing the same game for it to change the name. From 1-100.\n**!template**: Sets the template. Ex: `!template X - Y`. If no new template if provided it will reply with the currently set template.")
 			}
-		}
-		else if (messageL==="!showhyphen"){
-			if(message.member.hasPermission("MANAGE_CHANNELS")){
-				if(message.member.voiceChannel){
-					if(channels[message.member.voiceChannelID]){
-						if(channels[message.member.voiceChannelID][3]){
-							delete channels[message.member.voiceChannelID][3]
-							message.reply("Hyphen will be shown in `"+channels[message.member.voiceChannelID][0]+"`.")
-						}else{
-							channels[message.member.voiceChannelID][3]=1
-							message.reply("Hyphen will no longer be shown in `"+channels[message.member.voiceChannelID][0]+"`. ")
-						}
-						autosave()
-					}else{
-						message.reply("Please run `!addvc` first.")
-					}
-				}else{
-					message.reply("You must be in a voice channel to use this command.")
-				}
-			}else{
-				message.reply("You need `manage_channels` permission to do this.")
-			}
-		}
-		else if(messageL==="!majority"){
-			message.reply("Invalid syntax. Example: `!majority 50`")
-		}
-		else if(messageL.indexOf("!majority ")===0){
-			if(message.member.hasPermission("MANAGE_CHANNELS")){
-				if(message.member.voiceChannel){
-					if(channels[message.member.voiceChannelID]){
-						var majority=parseInt(messageL.substr(10))
-						if(majority){
-							if(majority>0 && majority <101){
-								channels[message.member.voiceChannelID][1]=majority/100
-								message.reply("Set majority for channel `"+channels[message.member.voiceChannelID][0]+"` to "+majority)
-								autosave()
-							}else{
-								message.reply("Invalid input. Number must be between 1 and 100.")
-							}
-						}else{
-							message.reply("Invalid input.")
-						}
-					}else{
-						message.reply("Please run `!addvc` first.")
-					}
-				}else{
-					message.reply("You must be in a voice channel to use this command!")
-				}
-			}else{
-				message.reply("You need `manage_channels` permission to do this.")
-			}
-		}
-		else if (messageL==="!help"){
-			message.channel.send("__Channel Changer Help__\n*The purpose of Channel Changer is to add what game you're playing to your connected voice channel's name.*\n**!addvc**: Adds your voice channel to be renamed.\n**!removevc**: Removes your voice channel from the list.\n**!majority**: Sets what percentage of people have to be playing the same game for it to change the name. From 1-100.\n**!gameonly**: Voice channel name will be set to just the title of the game, not hyphenated.\n**!showhyphen**: Toggles showing the hyphen (`-`) between the voice channel name and the game title.")
 		}
 	}
 })
@@ -268,4 +267,5 @@ client.on("guildCreate", guild=>{
 })
 
 process.on('unhandledRejection', function (err) {
+
 }); 
