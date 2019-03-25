72be5505d26fdc0d887d2c3807d78c3895fd5f92
Jared <7060603@hotmail.ca>
2019-03-25 16:05:32 -0400

    bug fixes and some tweaks
    - fixed bug where bots were being counted as users
    - removed support for archaic channel configurations
    - running !majority now shows the current value of majority
    - changed the way the bot checks if it has permission to channel.manageable instead of members.me.hasPermission
    - "ready" event now only shows the number of guilds instead of listing all guild names and id's
    - replaced the whole 'nogame' thing with undefined

--- channelchanger.js
+++ channelchanger.js
@@ -3,19 +3,17 @@ const jsonfile = require('jsonfile');
 
 const tokens = require('./tokens.js');
 var channels=require("./channels.json")
-var noFlyList=["nogame","Spotify"] // dont set the channel name to these
+var noFlyList=[undefined,"Spotify"] // dont set the channel name to these
 var changes=false //whether or not there have been unsaved changes made to the database
 
 const client = new Discord.Client();
 
 client.on('ready', () => {
-	console.log('Ready!');
+	console.log('Logged in as '+client.user.username);
 	client.user.setPresence({ game: { name: "!help - Changing Channels", type: 0 } });
-	//list connected guilds and their ID's
-	client.guilds.forEach(guild=>{
-		console.log(guild.name+" id: "+guild.id)
-	})
-	console.log(Object.keys(channels).length) // count managed channels
+	// Log some statistics
+	console.log("Guilds: "+client.guilds.size)
+	console.log("Channels in database: "+Object.keys(channels).length) // count managed channels
 });
 
 client.login(tokens.bot_token);
@@ -46,67 +44,57 @@ function save(){
 }
 
 /*
-* calculates returns what to set the channel name to
+* calculates and returns what to set the channel name to.
+* A sorting algorithm for the most part.
 * channel: the voice channel to calculate the majority game in
 * majorityPercent: the `!majority` value for the channel, as a decimal
 */
 function majority(channel,majorityPercent){
-	var games = {"nogame":0};
-	var majorityName="";
-	var majorityNumber=0;
+	var games = {}; // title : count
+	var majorityName=""; // after sorting, this is the most played game title
+	var majorityNumber=0; // after sorting, this is how many users are playing it
+	var userCount=0; // Number of non-bot users
 	channel.members.forEach(function(member){
 		if(!member.user.bot){ // ignore bots
+			userCount++;
 			if(member.presence.game){
-				games[member.presence.game.name]=(games[member.presence.game.name] || 0) + 1;
+				games[member.presence.game.name]=((games[member.presence.game.name] || 0) + 1);
 				if(games[member.presence.game.name]>majorityNumber){
 					majorityName=member.presence.game.name;
 					majorityNumber=games[member.presence.game.name];
 				}
-			}else{
-				games["nogame"]++
 			}
 		}
 	})
-	if((majorityNumber / channel.members.size) > majorityPercent){
+	if((majorityNumber / userCount) > majorityPercent){ // if we have a majority over the threshold
 		return(majorityName)
 	}else{
-		return("nogame")
+		return;
 	}
 }
-
-// Checks and sets the name of a voice channel. The core functionality.
+// Checks and sets the name of a voice channel.
 // channel: the voice channel in question.
 function scanOne(channel){
-	var channelConfig=channels[channel.id] //Store the channel's settings in a variable to prevent crashes
-	if(!channelConfig){
+	var channelConfig=channels[channel.id] // channel settings
+	if(!channelConfig){ // if the channel isnt in the database
 		console.log("Channel passed to function scanOne does not exist in database")
 		return;
 	}
 	if(channel){
 		if(channel.manageable){ //if the bot has permission to change the name
+			var newTitle=channelConfig[0];
 			if(channel.members.size>0){ // if anyone is in the channel
 				var gameTitle=majority(channel, channelConfig[1] || 0.5);
-				var newTitle="";
 				if(!noFlyList.includes(gameTitle)){
-					if(channelConfig[2]===1){  // "gameonly" archaic setting
-						newTitle=(gameTitle)
-					}else if(channelConfig[3]===1){ // "showhyphen" archaic setting
-						newTitle=(channelConfig[0]+" "+gameTitle)
-					}else if(channelConfig[2]){ //Template setting
+					if(channelConfig[2]){ //Template setting
 						newTitle=(channelConfig[2].replace(/X/,channelConfig[0]).replace(/Y/,gameTitle))
-					}else{
+					}else{ // use default
 						newTitle=(channelConfig[0] + " - " + gameTitle)
 					}
-				}else{
-					newTitle=(channelConfig[0]) // Set it back to normal
 				}
-				if(channel.name!==newTitle){
-					channel.setName(newTitle)
-				}
-			}else{
-				if(channel.name!==channelConfig[0]){
-					channel.setName(channelConfig[0])
-				}
+			}
+			if(channel.name!==newTitle){
+				channel.setName(newTitle)
 			}
 		}
 	}else{
@@ -147,9 +135,9 @@ client.on('message', message =>{
 		if(message.content[0]==="!"){
 			var messageL=message.content.toLowerCase()
 			if (messageL==="!addvc"){
-				if(message.guild.me.hasPermission("MANAGE_CHANNELS")){
-					if(message.member.hasPermission("MANAGE_CHANNELS")){
-						if (message.member.voiceChannel){
+				if(message.member.hasPermission("MANAGE_CHANNELS")){
+					if (message.member.voiceChannel){
+						if(message.member.voiceChannel.manageable){
 							if (!channels[message.member.voiceChannel.id]){
 								channels[message.member.voiceChannel.id]=[message.member.voiceChannel.name];
 								autosave()
@@ -159,35 +147,33 @@ client.on('message', message =>{
 								message.reply("`"+channels[message.member.voiceChannel.id][0]+"` is already on my list.")
 							}
 						}else{
-							message.reply("You must be in a voice channel to use this command.")
+							message.reply("I need `manage_channels` permission to do this.")
 						}
 					}else{
-						message.reply("You need `manage_channels` permission to do this.")
+						message.reply("You must be in a voice channel to use this command.")
 					}
 				}else{
-					message.reply("I need `manage_channels` permission to do this.")
+					message.reply("You need `manage_channels` permission to do this.")
 				}
 			}
 			else if(messageL==="!removevc"){
-				if(message.guild.me.hasPermission("MANAGE_CHANNELS")){
-					if(message.member.hasPermission("MANAGE_CHANNELS")){
-						if (message.member.voiceChannel){
-							if (channels[message.member.voiceChannel.id]){
+				if(message.member.hasPermission("MANAGE_CHANNELS")){
+					if (message.member.voiceChannel){
+						if (channels[message.member.voiceChannel.id]){
+							if(message.member.voiceChannel.manageable){
 								message.member.voicenewTitle=(channels[message.member.voiceChannelID][0])
-								delete channels[message.member.voiceChannelID];
-								autosave()
-								message.reply("Successfully removed `"+message.member.voiceChannel.name+"` from my list.")
-							}else{
-								message.reply("`"+message.member.voiceChannel.name+"` was not on my list.")
 							}
+							delete channels[message.member.voiceChannelID];
+							autosave()
+							message.reply("Successfully removed `"+message.member.voiceChannel.name+"` from my list.")
 						}else{
-							message.reply("You must be in a voice channel to use this command!")
+							message.reply("`"+message.member.voiceChannel.name+"` was not on my list.")
 						}
 					}else{
-						message.reply("You need `manage_channels` permission to do this.")
+						message.reply("You must be in a voice channel to use this command!")
 					}
 				}else{
-					message.reply("I need `manage_channels` permission to do this.")
+					message.reply("You need `manage_channels` permission to do this.")
 				}
 			}
 			else if(messageL==="!gameonly"){
@@ -206,8 +192,7 @@ client.on('message', message =>{
 							var newTemplate=message.content.substr(10).trim()
 							if(newTemplate.length<100){
 								if(newTemplate.includes("Y")){
-									channels[message.member.voiceChannelID][2]=newTemplate
-									delete channels[message.member.voiceChannelID][3] //Get rid of archaic stuff. Line not needed if using fresh database.
+									channels[message.member.voiceChannelID][2]=newTemplate;
 									message.reply("The template for `"+channels[message.member.voiceChannelID][0]+"` is now "+newTemplate)
 									scanOne(message.member.voiceChannel)
 									if(newTemplate.includes(message.member.voiceChannel.name)){
@@ -234,7 +219,15 @@ client.on('message', message =>{
 				message.reply("`!showhyphen` has been replaced with `!template`. Check `!help` for details.")
 			}
 			else if(messageL==="!majority"){
-				message.reply("Missing value. Example: `!majority 50`")
+				if(message.member.voiceChannel){
+					if(channels[message.member.voiceChannelID]){
+						if(channels[message.member.voiceChannelID][1]){
+							message.reply("Majority for `"+channels[message.member.voiceChannelID][0]+"`: "+channels[message.member.voiceChannelID][1])
+						}else{
+							message.reply("Majority for `"+channels[message.member.voiceChannelID][0]+"`: 50%")
+						}
+					}
+				}
 			}
 			else if(messageL.indexOf("!majority ")===0){
 				if(message.member.hasPermission("MANAGE_CHANNELS")){
